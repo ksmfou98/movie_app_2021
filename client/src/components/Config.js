be728c0551f0a538ab872eea021f74a0c537a706
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-13 15:28:52 +0900

    Implements :  https://www.themoviedb.org/ API 불러오기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https//api.themoviedb.org/3/";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+// api 키 : d489093209af90c05ab7d4cff29f4997
