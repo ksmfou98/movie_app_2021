6005b17d8265c84c1007c76997dda6d2c78d17d3
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-24 17:51:49 +0900

    Implements : 좋아요 추가/삭제 기능

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,11 +1,17 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./favorite.css";
+import { Popover } from "antd";
+import { IMAGE_BASE_URL } from "../../Config";
 
 const FavoritePage = () => {
   const [Favorites, setFavorites] = useState([]);
 
   useEffect(() => {
+    fetchFavoredMovie();
+  }, []);
+
+  const fetchFavoredMovie = () => {
     Axios.post("/api/favorite/getFavoredMovie", {
       userFrom: localStorage.getItem("userId"),
     }).then((response) => {
@@ -16,7 +22,53 @@ const FavoritePage = () => {
         alert("영화 정보를 가져오는데 실패 했습니다.");
       }
     });
-  }, []);
+  };
+
+  const onClickDelete = (movieId, userFrom) => {
+    const variables = {
+      movieId,
+      userFrom,
+    };
+
+    Axios.post("/api/favorite/removeFromFavorite", variables).then(
+      (response) => {
+        if (response.data.success) {
+            fetchFavoredMovie();
+        } else {
+          alert("리스트에서 지우는데 실패했습니다.");
+        }
+      }
+    );
+  };
+
+  const renderCards = Favorites.map((favorite, index) => {
+    const content = (
+      <div>
+        {favorite.moviePost ? (
+          <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} />
+        ) : (
+          "no image"
+        )}
+      </div>
+    );
+
+    return (
+      <tr key={index}>
+        <Popover content={content} title={`${favorite.movieTitle}`}>
+          <td>{favorite.movieTitle}</td>
+        </Popover>
+
+        <td>{favorite.movieRunTime}</td>
+        <td>
+          <button
+            onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}
+          >
+            Remove
+          </button>
+        </td>
+      </tr>
+    );
+  });
 
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
@@ -31,19 +83,7 @@ const FavoritePage = () => {
             <td>Remove from favorites</td>
           </tr>
         </thead>
-        <tbody>
-          {Favorites.map((favorite, index) => (
-            <tr key={index}>
-                <td>{favorite.movieTitle}</td>
-                <td>{favorite.movieRunTime}</td>
-                <td>
-                    <button>
-                        Remove
-                    </button>
-                </td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{renderCards}</tbody>
       </table>
     </div>
   );
